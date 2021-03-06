52b3285a73df840c4e541202af373de2bf96d807
ClicShopping <Yae8mr2o*>
2021-03-06 10:37:53 +0100

    update

--- sources/template/Default/modules/modules_front_page/fp_upcoming_products.php
+++ sources/template/Default/modules/modules_front_page/fp_upcoming_products.php
@@ -29,7 +29,7 @@
       $this->title = CLICSHOPPING::getDef('module_front_page_upcoming_products_title');
       $this->description = CLICSHOPPING::getDef('module_front_page_upcoming_products_description');
 
-      if (defined('MODULE_FRONT_PAGE_UPCOMING_PRODUCTS_STATUS')) {
+      if (\defined('MODULE_FRONT_PAGE_UPCOMING_PRODUCTS_STATUS')) {
         $this->sort_order = MODULE_FRONT_PAGE_UPCOMING_PRODUCTS_SORT_ORDER;
         $this->enabled = (MODULE_FRONT_PAGE_UPCOMING_PRODUCTS_STATUS == 'True');
       }
@@ -144,7 +144,7 @@
     }
 
     public function check() {
-      return defined('MODULE_FRONT_PAGE_UPCOMING_PRODUCTS_STATUS');
+      return \defined('MODULE_FRONT_PAGE_UPCOMING_PRODUCTS_STATUS');
     }
 
     public function install() {
